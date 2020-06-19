04f1ee7768f2daae4ebad2fbda3d3aa00407d875
Jeroen Boersma <jeroen@elgentos.nl>
2020-06-19 12:52:11 +0200

    Optimized redirect observer
    * optimized order of calls, return early
    * splitted into multiple methods
    + created tests

--- src/Observer/Redirect.php
+++ src/Observer/Redirect.php
@@ -2,8 +2,11 @@
 
 namespace Linktracker\Tracking\Observer;
 
+use Linktracker\Tracking\Api\ConfigInterface;
 use Linktracker\Tracking\Api\ConfigInterface as TrackingConfig;
 use Linktracker\Tracking\Model\CookieInterface;
+use Magento\Framework\App\HttpRequestInterface;
+use Magento\Framework\App\Response\HttpInterface;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 
@@ -14,11 +17,7 @@ class Redirect implements ObserverInterface
      */
     private $cookie;
 
-    private $response;
-
-    private $request;
-
-    private $doRedirect = true;
+    private $runOnce = false;
 
     public function __construct(
         CookieInterface $cookie
@@ -28,27 +27,57 @@ class Redirect implements ObserverInterface
 
     public function execute(Observer $observer)
     {
-        $this->request = $observer->getRequest();
-        $this->response = $observer->getControllerAction()->getResponse();
-
-        if ($this->request->getMethod() != 'GET') {
+        if ($this->runOnce) {
             return;
         }
 
-        $trackerId = $this->request->getParam(TrackingConfig::TRACKING_REQUEST_PARAMETER);
-        if (! $trackerId || ! $this->doRedirect) {
+        /** @var HttpRequestInterface $request */
+        $request = $observer->getEvent()
+                ->getData('request');
+
+        if (! $request->isGet()) {
             return;
         }
 
+        $trackerId = $request->getParam(TrackingConfig::TRACKING_REQUEST_PARAMETER);
+        if (! $trackerId) {
+            return;
+        }
         $this->cookie->setValue($trackerId);
 
-        $this->request->setDispatched(false);
-        $this->setRedirect($this->request->getOriginalPathInfo());
-        $this->doRedirect = false;
+        $request->setDispatched(false);
+
+        /** @var HttpInterface $response */
+        $response = $observer->getEvent()
+                ->getData('controller_action')
+                ->getResponse();
+
+        $this->setRedirect($request, $response);
+        $this->runOnce = true;
     }
 
-    public function setRedirect($redirectUrl)
+    public function setRedirect(HttpRequestInterface $request, HttpInterface $response): void
     {
-        $this->response->setRedirect($redirectUrl, 301)->sendResponse();
+        $url = $this->removeLinkTrackerParam($request->getOriginalPathInfo());
+
+        $response->setRedirect($url, 301);
+        $response->sendResponse();
+    }
+
+    /**
+     * @param $pathInfo
+     * @return string|string[]|null
+     */
+    public function removeLinkTrackerParam($pathInfo): string
+    {
+        return rtrim(preg_replace(
+            '#' .
+            '(&|\?)' .
+            preg_quote(ConfigInterface::TRACKING_REQUEST_PARAMETER, '#') .
+            '=[^&]+(&|$)' .
+            '#',
+            '\\1',
+            $pathInfo
+        ), '?&');
     }
 }
---
+++ tests/Observer/RedirectTest.php
@@ -0,0 +1,225 @@
+<?php
+
+namespace Linktracker\Tracking\Observer;
+
+use Linktracker\Tracking\Api\ConfigInterface;
+use Linktracker\Tracking\Model\CookieInterface;
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Request\Http;
+use Magento\Framework\App\Response\HttpInterface;
+use Magento\Framework\Event;
+use Magento\Framework\Event\Observer;
+use PHPUnit\Framework\TestCase;
+
+class RedirectTest extends TestCase
+{
+    /**
+     * @var CookieInterface|\PHPUnit\Framework\MockObject\MockObject
+     */
+    private $cookieMock;
+    /**
+     * @var Observer|\PHPUnit\Framework\MockObject\MockObject
+     */
+    private $observerMock;
+    /**
+     * @var Redirect
+     */
+    private $redirect;
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject
+     */
+    private $requestMock;
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject
+     */
+    private $eventMock;
+    /**
+     * @var HttpInterface|\PHPUnit\Framework\MockObject\MockObject
+     */
+    private $responseMock;
+
+    protected function setUp(): void
+    {
+        $this->cookieMock = $this->createMock(CookieInterface::class);
+        $this->responseMock = $this->createMock(HttpInterface::class);
+
+        $this->observerMock = $this->getMockBuilder(Observer::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->requestMock = $this->getMockBuilder(Http::class)
+                ->disableOriginalConstructor()
+                ->getMock();
+
+        $this->eventMock = $this->getMockBuilder(Event::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        // Link common objects
+        $this->observerMock->method('getEvent')
+                ->willReturn($this->eventMock);
+
+        $this->redirect = new Redirect(
+            $this->cookieMock
+        );
+    }
+
+    public function testSetRedirect()
+    {
+        $redirect = $this->redirect;
+
+        $requestMock = $this->requestMock;
+        $responseMock = $this->responseMock;
+
+        $requestMock->expects($this->once())
+                ->method('getOriginalPathInfo')
+                ->willReturn('/something?' . ConfigInterface::TRACKING_REQUEST_PARAMETER . '=12345');
+
+        $responseMock->expects($this->once())
+                ->method('setRedirect')
+                ->with('/something', 301);
+
+        $responseMock->expects($this->once())
+                ->method('sendResponse');
+
+        $redirect->setRedirect($requestMock, $responseMock);
+    }
+
+    public function testExecuteWithValue()
+    {
+        $redirect = $this->redirect;
+        $cookieMock = $this->cookieMock;
+
+        $observerMock = $this->observerMock;
+        $eventMock = $this->eventMock;
+
+        $requestMock = $this->requestMock;
+        $responseMock = $this->responseMock;
+
+        $controllerActionMock = $this->getMockForAbstractClass(
+            Action::class,
+            [],
+            '',
+            false,
+            true,
+            true,
+            ['getResponse']
+        );
+
+        $controllerActionMock->expects($this->once())
+                ->method('getResponse')
+                ->willReturn($responseMock);
+
+        $requestMock->expects($this->once())
+                ->method('isGet')
+                ->willReturn(true);
+
+        $requestMock->expects($this->once())
+                ->method('getParam')
+                ->with(ConfigInterface::TRACKING_REQUEST_PARAMETER)
+                ->willReturn('12345');
+
+        $eventMock->expects($this->exactly(2))
+                ->method('getData')
+                ->withConsecutive(['request'], ['controller_action'])
+                ->willReturn($requestMock, $controllerActionMock);
+
+
+        $cookieMock->expects($this->once())
+                ->method('setValue')
+                ->with('12345');
+
+        $redirect->execute($observerMock);
+
+        return $redirect;
+    }
+
+    /**
+     * @depends testExecuteWithValue
+     *
+     * @param Redirect $redirect
+     */
+    public function testExecuteTwiceIsIgnored(Redirect $redirect)
+    {
+        $observerMock = $this->observerMock;
+
+        $observerMock->expects($this->never())
+                ->method('getEvent');
+
+        $redirect->execute($observerMock);
+    }
+
+    public function testExecuteNotGet()
+    {
+        $redirect = $this->redirect;
+        $cookieMock = $this->cookieMock;
+
+        $observerMock = $this->observerMock;
+        $eventMock = $this->eventMock;
+        $requestMock = $this->requestMock;
+
+        $eventMock->method('getData')
+                ->with('request')
+                ->willReturn($requestMock);
+
+        $requestMock->expects($this->once())
+                ->method('isGet')
+                ->willReturn(false);
+
+        $requestMock->expects($this->never())
+                ->method('getParam');
+
+        $cookieMock->expects($this->never())
+                ->method('setValue');
+
+        $redirect->execute($observerMock);
+    }
+
+    public function testExecuteNoGetParameter()
+    {
+        $redirect = $this->redirect;
+        $cookieMock = $this->cookieMock;
+
+        $observerMock = $this->observerMock;
+        $eventMock = $this->eventMock;
+        $requestMock = $this->requestMock;
+
+        $eventMock->method('getData')
+            ->with('request')
+            ->willReturn($requestMock);
+
+        $requestMock->method('isGet')
+            ->willReturn(true);
+
+        $requestMock->expects($this->once())
+            ->method('getParam')
+            ->willReturn(null);
+
+        $cookieMock->expects($this->never())
+            ->method('setValue');
+
+        $redirect->execute($observerMock);
+    }
+
+    public function dataProviderForRemoveLinks(): array
+    {
+        return [
+            ['/something?' . ConfigInterface::TRACKING_REQUEST_PARAMETER . '=12345', '/something'],
+            ['/something?other=something&' . ConfigInterface::TRACKING_REQUEST_PARAMETER . '=12345', '/something?other=something'],
+            ['/something?' . ConfigInterface::TRACKING_REQUEST_PARAMETER . '=12345&other=something', '/something?other=something'],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForRemoveLinks
+     *
+     * @param string $input
+     * @param string $expected
+     */
+    public function testRemoveLinkTrackerParam(string $input, string $expected)
+    {
+        $redirect = $this->redirect;
+        $this->assertSame($expected, $redirect->removeLinkTrackerParam($input));
+    }
+
+}
