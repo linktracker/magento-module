947baa0ec226f0b4dfc2c96eeb321b8e426255e1
Jeroen Boersma <jeroen@elgentos.nl>
2020-06-19 21:10:32 +0200

    Added other params to configuration interface

--- src/Api/ConfigInterface.php
+++ src/Api/ConfigInterface.php
@@ -4,16 +4,22 @@ namespace Linktracker\Tracking\Api;
 
 interface ConfigInterface
 {
-    public const TRACKING_TABLE = 'linktracker_tracking';
+    public const TRACKING_TABLE             = 'linktracker_tracking';
 
-    public const STATUS_NEW = 1;
-    public const STATUS_SEND = 2;
-    public const STATUS_RESEND = 3;
-    public const STATUS_FAILED = 4;
+    public const STATUS_NEW                 = 1;
+    public const STATUS_SEND                = 2;
+    public const STATUS_RESEND              = 3;
+    public const STATUS_FAILED              = 4;
 
-    public const COOKIE_NAME = 'ltt-cookie';
-    public const COOKIE_DURATION = 86400 * 30; //in seconds, 30 days
+    public const COOKIE_NAME                = 'ltt-cookie';
+    public const COOKIE_DURATION            = 86400 * 30; //in seconds, 30 days
 
     public const TRACKING_REQUEST_PARAMETER = 'lt_clickid';
+
+
+    public const API_CONNECTION_TIMEOUT     = 3;
+    public const API_PARAM_ORDER_ID         = 'order_id';
+    public const API_PARAM_AMOUNT           = 'order_amount';
+    public const API_PARAM_ID               = 'click_id';
 
 }
