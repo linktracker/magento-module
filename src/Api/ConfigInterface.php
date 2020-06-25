8df8046419c857a0599eb39a45b0b68186159ef5
Jeroen Boersma <jeroen@elgentos.nl>
2020-06-25 15:09:04 +0200

    Updated cookie name

--- src/Api/ConfigInterface.php
+++ src/Api/ConfigInterface.php
@@ -11,11 +11,10 @@ interface ConfigInterface
     public const STATUS_RESEND              = 3;
     public const STATUS_FAILED              = 4;
 
-    public const COOKIE_NAME                = 'ltt-cookie';
+    public const COOKIE_NAME                = 'lt_clickid';
     public const COOKIE_DURATION            = 86400 * 30; //in seconds, 30 days
 
     public const TRACKING_REQUEST_PARAMETER = 'lt_clickid';
-
 
     public const API_CONNECTION_TIMEOUT     = 3;
     public const API_PARAM_ORDER_ID         = 'order_id';
