067b744b25fa7c078b8e7c9482164d372758dde3
Jeroen Boersma <jeroen@elgentos.nl>
2020-09-23 15:37:29 +0200

    Updated test for batch send to reflect current execute state
    * added test for current state
    * need to bump major version for upcoming changes

--- tests/Model/BatchSendTest.php
+++ tests/Model/BatchSendTest.php
@@ -4,6 +4,7 @@ namespace Linktracker\Tracking\Model;
 
 use Linktracker\Tracking\Api\ConfigInterface as TrackingConfig;
 use Linktracker\Tracking\Api\Data\TrackingInterface;
+use Linktracker\Tracking\Api\Data\TrackingSearchResultInterface;
 use Linktracker\Tracking\Api\TrackingRepositoryInterface;
 use Magento\Framework\Api\SearchCriteria;
 use Magento\Framework\Api\SearchCriteriaBuilder;
@@ -24,34 +25,80 @@ class BatchSendTest extends TestCase
     /**
      * @var TrackingRepositoryInterface|\PHPUnit\Framework\MockObject\MockObject
      */
-    private $trackingReposityryMock;
+    private $trackingRepositoryMock;
     /**
      * @var SearchCriteriaBuilder|\PHPUnit\Framework\MockObject\MockObject
      */
-    private $searchCriteriaMock;
+    private $searchCriteriaBuilderMock;
 
 
     protected function setUp(): void
     {
         $this->sendInterfaceMock = $this->createMock(SendInterface::class);
-        $this->trackingReposityryMock = $this->createMock(TrackingRepositoryInterface::class);
-        $this->searchCriteriaMock = $this->getMockBuilder(SearchCriteriaBuilder::class)
+        $this->trackingRepositoryMock = $this->createMock(TrackingRepositoryInterface::class);
+        $this->searchCriteriaBuilderMock = $this->getMockBuilder(SearchCriteriaBuilder::class)
                 ->disableOriginalConstructor()
                 ->getMock();
 
         $this->batchSend = new BatchSend(
             $this->sendInterfaceMock,
-            $this->trackingReposityryMock,
-            $this->searchCriteriaMock
+            $this->trackingRepositoryMock,
+            $this->searchCriteriaBuilderMock
         );
     }
 
+    /**
+     * Test execute functionality
+     * Should do a search for tracking items
+     * Should hand over these to the send interface
+     * And should save the status
+     */
+    public function testExecute()
+    {
+        $batchSend = $this->batchSend;
+
+        $searchCriteriaMock = $this->createMock(SearchCriteria::class);
+        $trackingSearchResult = $this->createMock(TrackingSearchResultInterface::class);
+
+        $searchCriteriaBuilderMock = $this->searchCriteriaBuilderMock;
+        $repositoryMock = $this->trackingRepositoryMock;
+        $sendMock = $this->sendInterfaceMock;
+
+        $searchCriteriaBuilderMock->method('addFilter')
+                ->willReturnSelf();
+
+        $searchCriteriaBuilderMock->method('create')
+                ->willReturn($searchCriteriaMock);
+
+        $trackingItem = $this->createMock(TrackingInterface::class);
+
+        // Should do a search
+        $repositoryMock->expects($this->once())
+                ->method('getList')
+                ->with($searchCriteriaMock)
+                ->willReturn($trackingSearchResult);
+
+        // Should get items
+        // Hand over 2 items
+        $trackingSearchResult->expects($this->once())
+            ->method('getItems')
+            ->willReturn([$trackingItem, $trackingItem]);
+
+        // Should hand over the information to the send interface
+        $sendMock->expects($this->exactly(2))
+                ->method('sendTrackingData')
+                ->willReturn(true, false);
+
+        // I would love to test the updateStatus, but this is already tested in updateStatus
+
+        $batchSend->execute();
+    }
 
     public function testUpdateStatus()
     {
         $batchSend = $this->batchSend;
 
-        $repositoryMock = $this->trackingReposityryMock;
+        $repositoryMock = $this->trackingRepositoryMock;
         $trackingMock = $this->createMock(TrackingInterface::class);
 
         $trackingMock->expects($this->once())
@@ -69,7 +116,7 @@ class BatchSendTest extends TestCase
     {
         $batchSend = $this->batchSend;
 
-        $searchCriteriaBuilderMock = $this->searchCriteriaMock;
+        $searchCriteriaBuilderMock = $this->searchCriteriaBuilderMock;
 
         $searchCriteriaBuilderMock->expects($this->once())
                 ->method('addFilter')
