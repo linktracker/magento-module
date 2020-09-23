fa12844a8fb6927bfcc7223a52da9c4806a16838
Jeroen Boersma <jeroen@elgentos.nl>
2020-09-23 16:32:26 +0200

    Implemented timestamp as a parameter send over to the tracker

--- src/Model/Send.php
+++ src/Model/Send.php
@@ -31,7 +31,19 @@ class Send implements SendInterface
                 TrackingConfig::API_PARAM_ID => $tracking->getTrackingId(),
                 TrackingConfig::API_PARAM_ORDER_ID => $tracking->getOrderIncrementId(),
                 TrackingConfig::API_PARAM_AMOUNT => $tracking->getGrandTotal(),
+                TrackingConfig::API_PARAM_TIMESTAMP => $this->convertCreatedAtToTimestamp($tracking->getCreatedAt()),
             ]
         );
     }
+
+    public function convertCreatedAtToTimestamp(string $timestamp): string
+    {
+        $datetime = new \DateTimeImmutable($timestamp, new \DateTimeZone('UTC'));
+        $timezone = new \DateTimeZone(TrackingConfig::API_TIMESTAMP_TIMEZONE);
+
+        return $datetime->setTimezone($timezone)
+                ->format(TrackingConfig::API_TIMESTAMP_FORMAT);
+
+    }
+
 }
